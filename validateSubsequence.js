0c3ae52dc65ae0370bae333f88b2ae0be0d690d5
Biligma Oyola <sandylykova94@mail.ru>
2020-09-21 21:25:03 -0400

    added description

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -1,3 +1,11 @@
+// Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+
+// A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array.
+
+// array = [5, 1, 22, 25, 6, -1, 8, 10];
+// subsequence = [1, 6, -1, 10];
+// isValidSubsequence(array, subsequence);
+
 // O(n) time | O(1) space
 
 function isValidSubsequence(array, sequence) {
