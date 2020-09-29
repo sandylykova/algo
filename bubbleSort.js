0bdda75ea2016dfb007fe0936b4ac953d072b311
Biligma Oyola <sandylykova94@mail.ru>
2020-09-28 22:26:10 -0400

    added solution

---
+++ bubbleSort.js
@@ -0,0 +1,27 @@
+// Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort algorithm to sort the array.
+
+// Best: O(n) time | O(1) space
+// Average: O(n^2) time | O(1) space
+// Worst: O(n^2) time | O(1) space
+
+function bubbleSort(array) {
+  let sorted = false;
+	let counter = 0;
+	while (!sorted) {
+		sorted = true;
+		for (let i = 0; i < array.length - 1 - counter; i++) {
+			if (array[i] > array[i+1]) {
+				swap(i, i+1, array);
+				sorted = false;
+			}
+		}
+		counter++;
+	}
+	return array;
+}
+
+function swap(i, j, array) {
+	let temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
