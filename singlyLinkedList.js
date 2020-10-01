1925f4cab62db1ffd64da777c6e0890db64c1c20
Biligma Oyola <sandylykova94@mail.ru>
2020-10-01 17:46:40 -0400

    added push method

---
+++ singlyLinkedList.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    this.length++;
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    return this;
+  }
+}
