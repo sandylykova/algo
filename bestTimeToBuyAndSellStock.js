e5c5c0a496539eea6b7ab1b3bb8e5f4baef0aa9f
Biligma Oyola <sandylykova94@mail.ru>
2020-10-05 20:37:11 -0400

    added second solution

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -27,3 +27,18 @@ var maxProfit = function(prices) {
   }
   return profit > 0 ? profit : 0;
 };
+
+// Solution 2 O(n) time | O(1) space
+
+var maxProfit = function(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length; i++) {
+      if (minPrice > prices[i]) {
+          minPrice = prices[i];
+      } else if (prices[i] - minPrice > maxProfit) {
+          maxProfit = prices[i] -  minPrice;
+      }
+  }
+  return maxProfit;
+};
