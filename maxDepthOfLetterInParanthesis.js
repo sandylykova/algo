be159985227c4260b9980ddbaf4dad8825e36327
Biligma Oyola <sandylykova94@mail.ru>
2020-10-09 12:42:01 -0400

    added second version

--- maxDepthOfLetterInParanthesis.js
+++ maxDepthOfLetterInParanthesis.js
@@ -10,6 +10,8 @@
 // Output : 3
 // maxDepth("(b) ((c) ()") => -1;
 
+// Solution 1 - returns max depth
+
 const maxDepth = (str) => {
   if (str.length === 0) return 0;
   let stack = [];
@@ -35,3 +37,35 @@ const maxDepth = (str) => {
   }
   return stack.length > 0 ? -1 : s[s.length - 1];
 };
+
+// Solution 2 - returns string with max depth
+
+const maxDepth2 = (str) => {
+  if (str.length === 0) return 0;
+  let stack = [];
+  let count = 0;
+  let s = [];
+  for (let i = 0; i < str.length; i++) {
+    let curVal = str[i];
+    if (curVal === ' ') continue;
+    if (curVal === '(') {
+      stack.push(curVal);
+      count++;
+    } else if (curVal === ')') {
+      if (stack.length === 0) return -1;
+      stack.pop();
+      count--;
+    } else {
+      if (s.length === 0) {
+        s.push(curVal, count);
+      } else if (count > s[s.length - 1]) {
+        s.pop();
+        s.pop();
+        s.push(curVal, count);
+      }
+    }
+  }
+  return stack.length > 0 ? -1 : [s[s.length - 2]];
+};
+
+// maxDepth('( a(b) (c) (d(e(f)g)h) I (j(k)l)m)') => ['f']
