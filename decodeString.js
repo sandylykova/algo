5828a78d01721d5d2b4500b7d7a3438678ed6657
Biligma Oyola <sandylykova94@mail.ru>
2020-10-17 11:16:56 -0400

    added solution

---
+++ decodeString.js
@@ -0,0 +1,48 @@
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+// You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+// Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+// Input: s = "3[a]2[bc]"
+// Output: "aaabcbc"
+// Number could be more than 9!!!
+
+// Solution 1
+// Time Complexity: O(maxK⋅n), where maxK is the maximum value of k in the string s.
+// Space Complexity: O(maxK⋅n).
+
+var decodeString = function(s) {
+  let stack = [];
+  let str = '';
+  let ret = '';
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === ']') {
+        str = '';
+        while (stack[stack.length - 1] !== '[') {
+          let popped = stack.pop();
+          str += popped;
+        }
+        stack.pop();
+        let num = '';
+        while (stack[stack.length - 1] >=0 && stack[stack.length - 1] <= 9) {
+          num += stack.pop();
+        }
+        num = num.split("").reverse().join("");
+        while (num > 0) {
+            stack.push(str);
+            num--;
+        }
+    } else {
+      stack.push(char);
+    }
+  }
+  for (let i = 0; i < stack.length; i++) {
+    if (stack[i].length > 1) {
+      stack[i] = stack[i].split("").reverse().join("");
+    }
+    ret += stack[i];
+  }
+  return ret;
+};
