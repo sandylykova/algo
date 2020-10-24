a02e9c40451fcb6fec19b2517b2ed81108b9d33b
Biligma Oyola <sandylykova94@mail.ru>
2020-10-24 18:02:32 -0400

    added  second solution

--- smallestDifference.js
+++ smallestDifference.js
@@ -1,14 +1,14 @@
-Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the first position.
+// Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the first position.
 
-You can assume that there will only be one pair of numbers with the smallest
-difference.
+// You can assume that there will only be one pair of numbers with the smallest
+// difference.
 
-Sample Input:
+// Sample Input:
 
-a1 = [-1, 5, 10, 20, 28, 3]
-a2 = [26, 134, 135, 15, 17]
+// a1 = [-1, 5, 10, 20, 28, 3]
+// a2 = [26, 134, 135, 15, 17]
 
-Output: [28, 26]
+// Output: [28, 26]
 
 // Solution 1 O(n^2) time | O(1) space
 
@@ -20,11 +20,31 @@ function smallestDifference(arrayOne, arrayTwo) {
 			let dif = Math.abs(arrayOne[i] - arrayTwo[j]);
 			if (smallestDif > dif) {
 				smallestDif = dif;
-				arr = [arrayOne[i], arrayTwo[j]]
+				arr = [arrayOne[i], arrayTwo[j]];
 			}
 		}
 	}
 	return arr;
 }
 
-// Solution 2 O(n) time | O() space
+// Solution 2 O(nlog(n) + mlog(m)) time | O(1) space
+
+function smallestDifference(arrayOne, arrayTwo) {
+	arrayOne.sort((a, b) => a - b);
+	arrayTwo.sort((a, b) => a - b);
+	let p1 = 0;
+	let p2 = 0;
+	let minDif = Infinity;
+	let returnedVals;
+	while (p1 < arrayOne.length && p2 < arrayTwo.length) {
+		let dif = Math.abs(arrayOne[p1] - arrayTwo[p2]);
+		if (dif === 0) return [arrayOne[p1], arrayTwo[p2]];
+		if (dif < minDif) {
+			minDif = dif;
+			returnedVals = [arrayOne[p1], arrayTwo[p2]];
+		}
+		if (arrayOne[p1] < arrayTwo[p2]) p1++;
+		else p2++;
+	}
+	return returnedVals;
+}
