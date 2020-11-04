2c55e7c1d4958bf241ef01759a61cf2e6d962eb0
Biligma Oyola <sandylykova94@mail.ru>
2020-11-04 12:51:43 -0500

    added depth first and breadth first grapth traversals

--- grapthImplementation.js
+++ grapthImplementation.js
@@ -22,4 +22,56 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  DFTraversalRecursive(start) {
+    let result = [];
+    let visited = {};
+    const adjacencyList = this.adjacencyList;
+    function dfs(vertex) {
+      if (!vertex) return;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    }
+    dfs(start);
+    return result;
+  }
+  DFTraversalIterative(start) {
+    let stack = [start];
+    let visited = {};
+    let result = [];
+    let node;
+    visited[start] = node;
+    while (stack.length) {
+      node = stack.pop();
+      result.push(node);
+      this.adjacencyList[node].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+  }
+  BFTraversal(start) {
+    let q = [start];
+    let visited = {};
+    visited[start] = true;
+    let result = [];
+    let vertex;
+    while (q.length) {
+      vertex = q.shift();
+      result.push(vertex);
+      this.adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          q.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
