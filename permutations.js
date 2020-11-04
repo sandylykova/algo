82f49f6b370ebbb30a8d8c9dcdd194a752da0aaa
Biligma Oyola <sandylykova94@mail.ru>
2020-11-03 20:25:00 -0500

    added solution

---
+++ permutations.js
@@ -0,0 +1,27 @@
+// Given a collection of distinct integers, return all possible permutations.
+
+// Example:
+
+// Input: [1,2,3]
+// Output:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+// Solution O(n * !n) time | O(n * !n) space
+
+var permute = function(nums, set = [], answers = []) {
+  if (!nums.length) answers.push([...set]);
+
+  for (let i = 0; i < nums.length; i++) {
+    const newNums = nums.filter((n, index) => index !== i);
+    set.push(nums[i]);
+    permute(newNums, set, answers);
+    set.pop();
+  }
+};
