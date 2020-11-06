ced9fc4f1d886f6852f9298d335f52400b407dde
Biligma Oyola <sandylykova94@mail.ru>
2020-11-06 17:32:20 -0500

    added first solution

---
+++ longestConsecutiveSequence.js
@@ -0,0 +1,31 @@
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// Follow up: Could you implement the O(n) solution?
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+// Solution 1 O(nlog(n)) time | O(1) space
+
+var longestConsecutive = function(nums) {
+  if (nums.length == 0) {
+      return 0;
+  }
+  nums.sort((a, b) => a - b);
+  let count = 0;
+  let i = 1;
+  let curCount = 1;
+  while (i < nums.length) {
+      if (nums[i] != nums[i - 1]) {
+          if (nums[i] === nums[i - 1] + 1) {
+              curCount++;
+          } else {
+              count = Math.max(count, curCount);
+              curCount = 1;
+          }
+      }
+      i++;
+  }
+  return Math.max(count, curCount);
+};
