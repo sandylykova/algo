3d584d6002c60cb1dd097100cd79311b711d0886
Biligma Oyola <sandylykova94@mail.ru>
2020-11-09 17:24:43 -0500

    added second solution

--- climbingStairs.js
+++ climbingStairs.js
@@ -22,3 +22,18 @@ var climbStairs = function(n) {
   }
   return countStairs(n, {});
 };
+
+// Solution 1 O(n) time | O(1) space
+
+var climbStairs = function(n) {
+  if (n === 1) return 1;
+  let first = 1;
+  let second = 2;
+  let third;
+  for (let i = 3; i <= n; i++) {
+      third = first + second;
+      first = second;
+      second = third;
+  }
+  return second;
+};
