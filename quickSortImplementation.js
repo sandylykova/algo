8f6b3c9290393ab73bb0d5c137d1cd7e75db741c
Biligma Oyola <sandylykova94@mail.ru>
2020-11-12 13:45:15 -0500

    added quick sort implementation

---
+++ quickSortImplementation.js
@@ -0,0 +1,27 @@
+function swap(arr, j, i) {
+  let temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
+}
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  let pivot = arr[start];
+  let swapIndex = start;
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot < arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
