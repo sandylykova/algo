cb9d54310937f0bf7777d32cfda3361148b4683c
Biligma Oyola <sandylykova94@mail.ru>
2020-11-11 19:16:55 -0500

    added second solution

--- KthSmallestElementInaBST.js
+++ KthSmallestElementInaBST.js
@@ -22,3 +22,24 @@ var kthSmallest = function(root, k) {
   return values[k - 1];
 };
 
+// Solution 2
+
+var topKFrequent = function(nums, k) {
+  const freqMap = new Map();
+  const bucket = [];
+  const result = [];
+
+  for(let num of nums) {
+      freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+
+  for(let [num, freq] of freqMap) {
+      bucket[freq] = (bucket[freq] || new Set()).add(num);
+  }
+
+  for(let i = bucket.length - 1; i >= 0; i--) {
+      if (bucket[i]) result.push(...bucket[i]);
+      if (result.length === k) break;
+  }
+  return result;
+};
