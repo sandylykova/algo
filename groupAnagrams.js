52413a219ca96e5539b80f7ad27ef730652f1743
Biligma Oyola <sandylykova94@mail.ru>
2020-11-22 15:34:12 -0500

    added second solution

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -29,3 +29,18 @@ function groupAnagrams(words) {
   }
 	return result;
 }
+
+// Solution 2 O(w*n*log(n)) time | O(wn) space - where w is the number of words and n is the length of the longest word
+
+function groupAnagrams(words) {
+	let anagrams = {};
+	for (let word of words) {
+		let sortedWord = word.split('').sort().join('');
+		if (sortedWord in anagrams) {
+			anagrams[sortedWord].push(word);
+		} else {
+			anagrams[sortedWord] = [word];
+		}
+	}
+	return Object.values(anagrams);
+}
