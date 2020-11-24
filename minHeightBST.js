d097dff26ffbdc1ec3c04a7b72d12cd0781d1a4b
Biligma Oyola <sandylykova94@mail.ru>
2020-11-24 09:28:08 -0500

    added second solution

--- minHeightBST.js
+++ minHeightBST.js
@@ -44,3 +44,41 @@ class BST {
     }
   }
 }
+
+// Solution 2 O(n) time | O(n) space -  where n is the length of the array
+
+function minHeightBst(array) {
+  function constructMinHeightBst(array, start, end) {
+		if (start > end) return null;
+		let midIdx = Math.floor((start + end) / 2);
+		let bst = new BST(array[midIdx]);
+		bst.left = constructMinHeightBst(array, start, midIdx - 1);
+		bst.right = constructMinHeightBst(array, midIdx + 1, end);
+		return bst;
+	}
+	return constructMinHeightBst(array, 0, array.length - 1);
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+}
