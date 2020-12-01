9a1d4aa61b597e3d324e38930040883247c43c45
Biligma Oyola <sandylykova94@mail.ru>
2020-12-01 16:22:16 -0500

    added second solution

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -1,15 +1,17 @@
-Given a singly linked list, determine if it is a palindrome.
+// Given a singly linked list, determine if it is a palindrome.
 
-Example 1:
-Input: 1->2
-Output: false
+// Example 1:
+// Input: 1->2
+// Output: false
 
-Example 2:
-Input: 1->2->2->1
-Output: true
+// Example 2:
+// Input: 1->2->2->1
+// Output: true
+
+// Solution 1 O(n) time | O(n) space
 
 var isPalindrome = function(head) {
-  let arr = []
+  let arr = [];
   while (head) {
       arr.push(head.val);
       head = head.next;
@@ -23,3 +25,36 @@ var isPalindrome = function(head) {
   }
   return true;
 };
+
+// Solution 2 O(n) time | O(1) space
+
+var isPalindrome = function(head) {
+  if(head == null || head.next == null) return true;
+      let slow = head;
+      let fast = head;
+      while (fast.next != null && fast.next.next != null) {
+          slow = slow.next;
+          fast = fast.next.next;
+      }
+      slow.next = reverseList(slow.next);
+      slow = slow.next;
+      while (slow != null) {
+          if(head.val != slow.val) return false;
+          head = head.next;
+          slow = slow.next;
+      }
+      return true;
+};
+
+var reverseList = function(head) {
+      var pre = null;
+      var next = null;
+      while(head != null) {
+          next = head.next;
+          head.next = pre;
+          pre = head;
+          head = next;
+      }
+      return pre;
+};
+
