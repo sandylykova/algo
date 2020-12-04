3de1b0b0b45a641c7075ad89c5a23804b6334751
Biligma Oyola <sandylykova94@mail.ru>
2020-12-04 10:20:33 -0500

    added second solution

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -47,3 +47,31 @@ var mergeTwoLists = function(l1, l2) {
     }
     return dummyHead.next;
 };
+
+// Solution 2 O(n + m) time | O(1) space
+
+class LinkedList {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  function mergeLinkedLists(headOne, headTwo) {
+    let p1 = headOne;
+      let p1Prev = null;
+      let p2 = headTwo;
+      while (p1 !== null && p2 !== null) {
+          if (p1.value < p2.value) {
+              p1Prev = p1;
+              p1 = p1.next;
+          } else {
+              if (p1Prev !== null) p1Prev.next = p2;
+              p1Prev = p2;
+              p2 = p2.next;
+              p1Prev.next = p1;
+          }
+      }
+      if (p1 === null) p1Prev.next = p2;
+      return headOne.value < headTwo.value ? headOne : headTwo;
+  }
