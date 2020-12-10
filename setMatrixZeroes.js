55cc6aba7045144c2efc530c5472008afb838ae3
Biligma Oyola <sandylykova94@mail.ru>
2020-12-10 14:19:45 -0500

    added second solution

--- setMatrixZeroes.js
+++ setMatrixZeroes.js
@@ -39,3 +39,39 @@ function fillWithZeroesCol(matrix, index) {
       matrix[i][index] = 0;
   }
 }
+
+// Solution 2 O(n^2) time | O(1) space
+
+var setZeroes = function(matrix) {
+  let isCol = false;
+  let rows = matrix.length;
+  let cols = matrix[0].length;
+  for (let i = 0; i < rows; i++) {
+      if (matrix[i][0] === 0) isCol = true;
+      for (let j = 1; j < cols; j++) {
+          if (matrix[i][j] === 0) {
+              matrix[i][0] = 0;
+              matrix[0][j] = 0;
+          }
+      }
+  }
+  for (let i = 1; i < rows; i++) {
+      for (let j = 1; j < cols; j++) {
+          if (matrix[0][j] === 0 || matrix[i][0] === 0) {
+              matrix[i][j] = 0;
+          }
+      }
+  }
+  // See if the first row needs to be set to zero as well
+  if (matrix[0][0] === 0) {
+      for (let j = 0; j < cols; j++) {
+          matrix[0][j] = 0;
+      }
+  }
+  // See if the first column needs to be set to zero as well
+  if (isCol) {
+      for (let i = 0; i < rows; i++) {
+          matrix[i][0] = 0;
+      }
+  }
+};
