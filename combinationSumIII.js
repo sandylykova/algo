c4b39c8b5a5da0521ae1f419bee1d5c92321490e
Biligma Oyola <sandylykova94@mail.ru>
2020-12-09 19:10:55 -0500

    added first solution

---
+++ combinationSumIII.js
@@ -0,0 +1,35 @@
+// 216. Combination Sum III
+
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+// Only numbers 1 through 9 are used.
+// Each number is used at most once.
+// Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+// Input: k = 3, n = 7
+// Output: [[1,2,4]]
+// Explanation:
+// 1 + 2 + 4 = 7
+// There are no other valid combinations.
+
+// Solution 1
+
+var combinationSum3 = function(k, n) {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let result = [];
+  function backtrack(remaining, curr, index) {
+      if (remaining < 0) return;
+      if (remaining === 0 && curr.length === k) {
+          result.push(curr.slice());
+      }
+      if (curr.length > k) return;
+      for (let i = index; i < numbers.length; i++) {
+          curr.push(numbers[i]);
+          let newRemaining = remaining - numbers[i];
+          backtrack(newRemaining, curr, i + 1);
+          curr.pop();
+      }
+  }
+  backtrack(n, [], 0);
+  return result;
+};
