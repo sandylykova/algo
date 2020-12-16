c5881f73a935ae34ca1465d5b420f65bf398fa4f
Biligma Oyola <sandylykova94@mail.ru>
2020-12-16 18:39:19 -0500

    added second solution

--- arrayOfProducts.js
+++ arrayOfProducts.js
@@ -1,7 +1,7 @@
 Write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal to the product of every other number in the input array.
 Note that you're expected to solve this problem without using division.
 
-// Solution 1 O(n) space | O(n) time
+// Solution 1 O(n^2) space | O(n) time
 
 function arrayOfProducts(array) {
 	let result = [];
@@ -15,4 +15,27 @@ function arrayOfProducts(array) {
 		result.push(product);
 	}
 	return result;
+}
+
+// Solution 2 O(n) time | O(n) space
+
+function arrayOfProducts(array) {
+	let products = new Array(array.length).fill(1);
+	let leftProducts = new Array(array.length).fill(1);
+	let rightProducts = new Array(array.length).fill(1);
+
+  let leftRunningProduct = 1;
+	for (let i = 0; i < array.length; i++) {
+		leftProducts[i] = leftRunningProduct;
+		leftRunningProduct *= array[i];
+	}
+	let rightRunningProduct = 1;
+	for (let i = array.length - 1; i >= 0; i--) {
+		rightProducts[i] = rightRunningProduct;
+		rightRunningProduct *= array[i];
+	}
+	for (let i = 0; i < array.length; i++) {
+		products[i] = leftProducts[i] * rightProducts[i];
+	}
+	return products;
 }
