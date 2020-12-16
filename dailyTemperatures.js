e760f6be20701194b43330f16074f30cc86e1baa
Biligma Oyola <sandylykova94@mail.ru>
2020-12-16 14:12:10 -0500

    added second solution

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -22,3 +22,22 @@ var dailyTemperatures = function(T) {
   ans.push(0);
   return ans;
 };
+
+// Solution 2 O(n) time | O(n) space
+
+var dailyTemperatures = function(T) {
+  const result = Array(T.length).fill(0);
+  const stack = [];
+
+  for(let i = 0; i < T.length; i++) {
+      let top = stack[stack.length-1];
+
+      while (stack.length && T[top] < T[i]) {
+          const idx = stack.pop();
+          result[idx] = i - idx;
+          top = stack[stack.length-1];
+      }
+      stack.push(i);
+  }
+  return result;
+};
