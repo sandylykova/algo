d8e278d7cea4ff0e0196b9c00e18f8b762ded605
Biligma Oyola <sandylykova94@mail.ru>
2021-01-10 11:35:20 -0500

    added second solution

--- populatingNextRigthPointersInEachNodeII.js
+++ populatingNextRigthPointersInEachNodeII.js
@@ -41,3 +41,27 @@ var connect = function(root) {
   }
   return root;
 };
+
+// Solution 2 O(n) time | O(n) space
+
+var connect = function(root) {
+    if (!root) return null;
+
+    let queue = [root];
+
+    while (queue.length) {
+        let n = queue.length;
+
+        for (let i = 0; i < n; i++) {
+            let node = queue.shift();
+
+            node.next = i === n - 1 ? null : queue[0];
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+    }
+
+    return root;
+};
