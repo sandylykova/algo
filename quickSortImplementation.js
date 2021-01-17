1d40b376f47ca4868d4a5da0463b60dab370d05c
Biligma Oyola <sandylykova94@mail.ru>
2021-01-17 13:14:27 -0500

    added second solution

--- quickSortImplementation.js
+++ quickSortImplementation.js
@@ -31,3 +31,31 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
   }
   return arr;
 }
+
+// Solution 2
+
+function quickSortHelper(array, startIdx, endIdx) {
+	if (startIdx >= endIdx) return;
+	let pivot = startIdx;
+	let left = startIdx + 1;
+	let right = endIdx;
+	while (left <= right) {
+		if (array[left] > array[pivot] && array[right] < array[pivot]) {
+			swap(left, right, array);
+		}
+		if (array[left] <= array[pivot]) left++;
+		if (array[right] >= array[pivot]) right--;
+	}
+	swap(pivot, right, array);
+	let leftSubarrayIsSmaller = right - 1 - startIdx < endIdx - (right + 1);
+	if (leftSubarrayIsSmaller) {
+		quickSortHelper(array, startIdx, right - 1);
+		quickSortHelper(array, right + 1, endIdx);
+	} else {
+		quickSortHelper(array, right + 1, endIdx);
+		quickSortHelper(array, startIdx, right - 1);
+	}
+}
+function swap(i, j, array) {
+	[array[i], array[j]] = [array[j], array[i]];
+}
