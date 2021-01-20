a8e23cabc3e7ef591f8bedd65915b8d8f9b1133b
Biligma Oyola <sandylykova94@mail.ru>
2021-01-19 19:40:29 -0500

    added first solution

---
+++ slidingWindowMaximum.js
@@ -0,0 +1,19 @@
+// 239. Sliding Window Maximum
+
+// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+// Solution 1 brute force
+
+var maxSlidingWindow = function(nums, k) {
+  let max = [];
+  for (let i = 0; i <= nums.length - k; i++) {
+      let currMax = -Infinity;
+      for (let j = i; j < i + k; j++) {
+          currMax = Math.max(currMax, nums[j]);
+      }
+      max.push(currMax);
+  }
+  return max;
+};
