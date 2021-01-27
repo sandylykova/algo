0ae6d0d509e47e7c58690b8ee146ac9ae46b7e1d
Biligma Oyola <sandylykova94@mail.ru>
2021-01-27 13:35:02 -0500

    added second solution

--- longestCommonSubsequence.js
+++ longestCommonSubsequence.js
@@ -23,3 +23,43 @@ function longestCommonSubsequence(str1, str2) {
 	}
 	return lcs[str2.length][str1.length];
 }
+
+// Solution 2 O(nm) time | O(nm) space
+
+function longestCommonSubsequence(str1, str2) {
+	let lcs = new Array(str2.length + 1).fill([null, 0, null, null]).map(() => new Array(str1.length + 1).fill([null, 0, null, null]));
+	findLongestCommonSubsequence(lcs, str1, str2);
+	return builtResult(lcs);
+}
+function builtResult(lcs) {
+	let ans = [];
+	let i = lcs.length - 1;
+	let j = lcs[0].length - 1;
+	while (i !== 0 && j !== 0) {
+		let currentEntry = lcs[i][j];
+		if (currentEntry[0]) {
+			ans.push(currentEntry[0]);
+		}
+		i = currentEntry[2];
+		j = currentEntry[3];
+	}
+	return ans.reverse();
+}
+function findLongestCommonSubsequence(lcs, str1, str2) {
+	for (let i = 1; i < str2.length + 1; i++) {
+		for (let j = 1; j < str1.length + 1; j++) {
+			if (str2[i - 1] === str1[j - 1]) {
+				let prev = lcs[i - 1][j - 1];
+				lcs[i][j] = [str2[i - 1], prev[1] + 1, i - 1, j - 1];
+			} else {
+				let up = lcs[i - 1][j];
+				let left = lcs[i][j - 1];
+				if (up[1] > left[1]) {
+					lcs[i][j] = [null, up[1], i - 1, j];
+				} else {
+					lcs[i][j] = [null, left[1], i, j - 1];
+				}
+			}
+		}
+	}
+}
