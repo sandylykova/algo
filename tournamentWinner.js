925bc86be0a114f612d7b12ca824e8b8ef7bf96c
Biligma Oyola <sandylykova94@mail.ru>
2021-02-18 16:08:59 -0500

    added time and space complexity

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -1,7 +1,7 @@
 // AlgoExpert: Tournament Winner
 // Difficulty: easy
 
-// Solution 1
+// Solution 1 O(n) time | O(k) space, where n is the number of competitions and k is the number of teams
 
 function tournamentWinner(competitions, results) {
 	let map = new Map();
