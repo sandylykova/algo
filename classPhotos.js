92dd95517e97b52e1269b31e9fb1694557ce9fd7
Biligma Oyola <sandylykova94@mail.ru>
2021-02-18 16:14:14 -0500

    added second solution

--- classPhotos.js
+++ classPhotos.js
@@ -1,7 +1,7 @@
 // AlgoExpert: Class Photos
 // Difficulty: easy
 
-// Solution 1 O(n(log(n))) time | O(n) space
+// Solution 1 O(nlog(n)) time | O(n) space
 
 function classPhotos(redShirtHeights, blueShirtHeights) {
 	redShirtHeights.sort((a, b) => a - b);
@@ -16,6 +16,24 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
 	}
 	for (let i = 0; i < frontRow.length; i++) {
 		if (frontRow[i] >= backRow[i]) return false;
+	}
+  return true;
+}
+
+// Solution 1 O(nlog(n)) time | O(1) space
+
+function classPhotos(redShirtHeights, blueShirtHeights) {
+  redShirtHeights.sort((a, b) => a - b);
+	blueShirtHeights.sort((a, b) => a - b);
+	let firstRowShirtColor = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+	for (let i = 0; i < redShirtHeights.length; i++) {
+		let redShirt = redShirtHeights[i];
+		let blueShirt = blueShirtHeights[i];
+		if (firstRowShirtColor === 'RED') {
+			if (redShirt >= blueShirt) return false;
+		} else {
+			if (blueShirt >= redShirt) return false;
+		}
 	}
   return true;
 }
