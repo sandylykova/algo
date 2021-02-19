db0ed13116afeaedde25b8c2f1d54357b4da59a7
Biligma Oyola <sandylykova94@mail.ru>
2021-02-19 13:47:05 -0500

    added second solution

--- staircaseTraversal.js
+++ staircaseTraversal.js
@@ -24,3 +24,22 @@ function staircaseTraversal(height, maxSteps) {
 	backtrack(0);
   return count;
 }
+
+// Solution 2 O(k * n) time | O(n) space, where k is the number of allowed steps and n is the height of the staircase
+
+function staircaseTraversal(height, maxSteps) {
+	let memo = new Map();
+	memo.set(0, 1);
+	memo.set(1, 1);
+  return numberOfWaysToTop(height, maxSteps, memo);
+}
+
+function numberOfWaysToTop(height, maxSteps, memo) {
+	if (memo.has(height)) return memo.get(height);
+	let numberOfWays = 0;
+	for (let i = 1; i <= Math.min(height, maxSteps); i++) {
+		numberOfWays += numberOfWaysToTop(height - i, maxSteps, memo);
+	}
+	memo.set(height, numberOfWays);
+	return numberOfWays;
+}
