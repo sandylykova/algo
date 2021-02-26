056b02f384e2abb66584eb032778aadfc6a1b842
Biligma Oyola <sandylykova94@mail.ru>
2021-02-25 19:03:28 -0500

    added second solution

--- findTheDuplicateNumber.js
+++ findTheDuplicateNumber.js
@@ -17,3 +17,20 @@ var findDuplicate = function(nums) {
     }
     return -1;
 };
+
+// Solution 2 O(n) time | O(1) space
+
+var findDuplicate = function(nums) {
+  let slow = nums[0];
+  let fast = nums[nums[0]];
+  while (slow !== fast) {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+  }
+  slow = 0;
+  while (slow !== fast) {
+      slow = nums[slow];
+      fast = nums[fast];
+  }
+  return slow;
+};
