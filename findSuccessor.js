2cc80df07b7deb7d7c6d20af32042fcd65c50c12
Biligma Oyola <sandylykova94@mail.ru>
2021-03-19 15:24:29 -0400

    added second solution

--- findSuccessor.js
+++ findSuccessor.js
@@ -31,3 +31,26 @@ function findSuccessor(tree, node) {
 	}
   return null;
 }
+
+// Solution 2 O(h) time | O(1) space
+
+function findSuccessor(tree, node) {
+	if (node.right !== null) return findLeftmostChild(node.right);
+	return findRightmostParent(node);
+}
+
+function findLeftmostChild(node) {
+	let currentNode = node;
+	while (currentNode.left !== null) {
+			currentNode = currentNode.left;
+	}
+	return currentNode;
+}
+
+function findRightmostParent(node) {
+	let currentNode = node;
+	while (currentNode.parent !== null && currentNode.parent.right === currentNode) {
+		currentNode = currentNode.parent;
+	}
+	return currentNode.parent;
+}
