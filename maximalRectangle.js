c7cf6d106548e2d684df83ef38854a5f9711c1a6
Biligma Oyola <sandylykova94@mail.ru>
2021-03-26 11:14:27 -0400

    added second solution

--- maximalRectangle.js
+++ maximalRectangle.js
@@ -43,3 +43,62 @@ var maximalRectangle = function(matrix) {
   }
   return max;
 };
+
+
+// Solution 2 O(nm) time | O(m) space
+
+var maximalRectangle = function(matrix) {
+    let max = 0;
+    let histogram = [];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (i === 0) {
+                histogram[j] = matrix[i][j] === '1' ? 1 : 0;
+            } else {
+                if (matrix[i][j] === '1') histogram[j] += 1;
+                else histogram[j] = 0;
+            }
+        }
+        max = Math.max(max, findMaxRectangle(histogram));
+    }
+    return max;
+};
+
+function findMaxRectangle(arr) {
+    let max = 0;
+    let leftSideLimit = [];
+    let rightSideLimit = [];
+    let s = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (s.length === 0) {
+            s.push(i);
+            leftSideLimit[i] = 0;
+        } else {
+            while (s.length && arr[s[s.length - 1]] >= arr[i]) {
+                s.pop();
+            }
+            if (s.length === 0) leftSideLimit.push(0);
+            else leftSideLimit.push(s[s.length - 1] + 1);
+            s.push(i);
+        }
+    }
+    s = [];
+    let rightLimit = arr.length - 1;
+    for (let i = rightLimit; i >= 0; i--) {
+        if (s.length === 0) {
+            s.push(i);
+            rightSideLimit[i] = rightLimit;
+        } else {
+            while (s.length && arr[s[s.length - 1]] >= arr[i]) {
+                s.pop();
+            }
+            if (s.length === 0) rightSideLimit[i] = rightLimit;
+            else rightSideLimit[i] = s[s.length - 1] - 1;
+            s.push(i);
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        max = Math.max(max, (rightSideLimit[i] - leftSideLimit[i] + 1) * arr[i]);
+    }
+    return max;
+}
