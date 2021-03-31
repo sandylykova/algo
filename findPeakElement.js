cbfb0b402b0a0c7288f75efb4ff39cc68d7ad7ac
Biligma Oyola <sandylykova94@mail.ru>
2021-03-31 16:42:50 -0400

    added second solution

--- findPeakElement.js
+++ findPeakElement.js
@@ -16,3 +16,21 @@ var findPeakElement = function(nums) {
   }
   return nums.length - 1;
 };
+
+// Solution 2 O(log(n)) time | O(1) space
+
+var findPeakElement = function(nums) {
+  if (nums.length === 0) return -1;
+  if (nums.length === 1) return 0;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (mid === 0 && nums[mid] > nums[mid + 1]) return mid;
+      else if (mid === nums.length - 1 && nums[mid] > nums[mid - 1]) return mid;
+      else if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) return mid;
+      else if (nums[mid] > nums[mid + 1]) right = mid - 1;
+      else left = mid + 1;
+  }
+  return -1;
+}
