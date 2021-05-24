0d34e3ffa117a1276f200c632f9e871c4bccc132
Biligma Oyola <sandylykova94@mail.ru>
2021-05-24 13:50:33 -0400

    added second solution

--- apartmentHunting.js
+++ apartmentHunting.js
@@ -57,3 +57,80 @@ function findClosestBlock(left, right, blocks, currIdx, req) {
 	else if (foundLeft === true && foundRight === false) return counterLeft;
 	else return Math.min(counterLeft, counterRight);
 }
+
+// Solution 2 O(m*n) time | O(m*n) space, where r is the number of the reqs, n is the number of the blocks
+
+function apartmentHunting(blocks, reqs) {
+	let leftSide = [];
+	let rightSide = [];
+	let reqsSet = new Set(reqs);
+	for (let i = 0; i < blocks.length; i++) {
+		let currentBlock = blocks[i];
+		let currentValues = [];
+		let idx = 0;
+		for (let req in currentBlock) {
+			if (!reqsSet.has(req)) continue;
+			if (i === 0) {
+				if (reqsSet.has(req)  && currentBlock[req] === true) {
+					currentValues[idx] = 0;
+				} else {
+					currentValues[idx] = Infinity;
+				}
+			} else {
+				let prevBlock = leftSide[i - 1];
+				if (reqsSet.has(req) && currentBlock[req] === true) {
+					currentValues[idx] = 0;
+				} else if (prevBlock[idx] === Infinity) {
+					currentValues[idx] = Infinity;
+				} else {
+					currentValues[idx] = prevBlock[idx] + 1;
+				}
+			}
+			idx++;
+		}
+		leftSide.push(currentValues);
+	}
+	for (let i = blocks.length - 1; i >= 0; i--) {
+		let currentBlock = blocks[i];
+		let currentValues = [];
+		let idx = 0;
+		for (let req in currentBlock) {
+			if (!reqsSet.has(req)) continue;
+			if (i === blocks.length - 1) {
+				if (reqsSet.has(req) && currentBlock[req] === true) {
+					currentValues[idx]  = 0;
+				} else {
+					currentValues[idx] = Infinity;
+				}
+			} else {
+				let prevBlock = rightSide[i + 1];
+				if (reqsSet.has(req) && currentBlock[req] === true) {
+					currentValues[idx] = 0;
+				} else if (prevBlock[idx] === Infinity) {
+					currentValues[idx] = Infinity;
+				} else {
+					currentValues[idx] = prevBlock[idx] + 1;
+				}
+			}
+			idx++;
+			rightSide[i] = currentValues;
+		}
+	}
+	let idx = null;
+	let minSteps = Infinity;
+	for (let i = 0; i < blocks.length; i++) {
+		let currLeft = leftSide[i];
+		let currRight = rightSide[i];
+		let currMin = -Infinity;
+		let currSteps = -Infinity;
+		for (let j = 0; j < reqs.length; j++) {
+			currMin = Math.min(currLeft[j], currRight[j]);
+			currSteps = Math.max(currSteps, currMin);
+		}
+		if (currSteps < minSteps) {
+			minSteps = currSteps;
+			idx = i;
+		}
+	}
+	return idx;
+}
