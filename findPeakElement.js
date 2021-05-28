47217ce7cd37a0d535fefff894b4b05a9143aae5
Biligma Oyola <sandylykova94@mail.ru>
2021-05-28 13:09:02 -0400

    added third solution

--- findPeakElement.js
+++ findPeakElement.js
@@ -39,7 +39,6 @@ var findPeakElement = function(nums) {
 
 var findPeakElement = function(nums) {
   if (nums.length === 0) return -1;
-  if (nums.length === 1) return 0;
   let l = 0, r = nums.length - 1;
   while (l < r) {
       let mid = Math.floor((l + r) / 2);
