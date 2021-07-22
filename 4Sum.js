ab580666c2b1cf41cde64ea21f1d847e55b2d8ee
Biligma Oyola <sandylykova94@mail.ru>
2021-07-22 10:42:36 -0400

    added second solution

--- 4Sum.js
+++ 4Sum.js
@@ -37,3 +37,36 @@ var fourSum = function(nums, target) {
   }
   return vals;
 };
+
+// Solution 2 (cleaner Solution 1) O(n^3) time | O(1) space
+
+var fourSum = function(nums, target) {
+  let values = [];
+  if (!nums || nums.length < 4) return values;
+  let len = nums.length;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < len - 3; i++) {
+      if (i > 0 && nums[i] === nums[i - 1]) continue;
+      for (let j = i + 1; j < len - 2; j++) {
+          if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+          let sum1 = nums[i] + nums[j];
+          let left = j + 1;
+          let right = len - 1;
+          while (left < right) {
+              let sum2 = sum1 + nums[left] + nums[right];
+              if (sum2 === target) {
+                  values.push([nums[i], nums[j], nums[left], nums[right]]);
+                  left++;
+                  right--;
+                  while (left < right && nums[left] === nums[left - 1]) left++;
+                  while (left < right && nums[right] === nums[right + 1]) right--;
+              } else if (sum2 > target) {
+                  right--;
+              } else {
+                  left++;
+              }
+          }
+      }
+  }
+  return values;
+};
