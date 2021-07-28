57843a2236e95151eab566da8b21b7f23d8b393b
Biligma Oyola <sandylykova94@mail.ru>
2021-07-28 10:42:28 -0400

    added first solution

---
+++ differentWaysToAddParentheses.js
@@ -0,0 +1,40 @@
+// 241. Different Ways to Add Parentheses
+
+// Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
+
+// Input: expression = "2*3-4*5"
+// Output: [-34,-14,-10,-10,10]
+// Explanation:
+// (2*(3-(4*5))) = -34
+// ((2*3)-(4*5)) = -14
+// ((2*(3-4))*5) = -10
+// (2*((3-4)*5)) = -10
+// (((2*3)-4)*5) = 10
+
+// Solution 1
+
+var diffWaysToCompute = function(expression) {
+  let result = [];
+  for (let i = 0; i < expression.length; i++) {
+      let char = expression[i];
+      if (char === '+' || char === '*' || char === '-') {
+          let a = expression.slice(0, i);
+          let b = expression.slice(i + 1);
+          let valsA = diffWaysToCompute(a);
+          let valsB = diffWaysToCompute(b);
+          for (let val1 of valsA) {
+              for (let val2 of valsB) {
+                  if (char === '+') {
+                      result.push(val1 + val2);
+                  } else if (char === '*') {
+                      result.push(val1 * val2);
+                  } else {
+                      result.push(val1 - val2);
+                  }
+              }
+          }
+      }
+  }
+  if (result.length === 0) result.push(Number(expression));
+  return result;
+};
