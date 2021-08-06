6ef3171313c16e66035017e30682feb63bcd4991
Biligma Oyola <sandylykova94@mail.ru>
2021-08-06 16:47:18 -0400

    added first solution

---
+++ validParenthesisString.js
@@ -0,0 +1,52 @@
+// 678. Valid Parenthesis String
+
+// Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+// The following rules define a valid string:
+
+// Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+// Any right parenthesis ')' must have a corresponding left parenthesis '('.
+// Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+// '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+// Input: s = "(*))"
+// Output: true
+
+// Solution 1 O(n*m) time | O(n + m) space
+
+var checkValidString = function(s) {
+  let stack = [];
+  let asterics = [];
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === '(') {
+          stack.push([s[i], i]);
+      } else if (s[i] === ')') {
+          if (stack.length && stack[stack.length - 1][0] === '(') stack.pop();
+          else if (asterics.length > 0) {
+              stack.pop();
+              asterics.pop();
+          }
+          else return false;
+      } else {
+          asterics.push(i);
+      }
+  }
+  if (stack.length === 0) return true;
+  else {
+      let i = 0;
+      if (asterics.length < stack.length) return false;
+      for (let [char, idx] of stack) {
+          let found = false;
+          for (let i = 0; i < asterics.length; i++) {
+              let val = asterics[i];
+              if (val > 0 && val > idx) {
+                  found = true;
+                  asterics[i] = -val;
+                  break;
+              }
+          }
+          if (!found) return false;
+      }
+  }
+  return true;
+};
