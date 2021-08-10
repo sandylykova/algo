58637e4e65dbb7bd50b72fb8a1a3c9d3352d7045
Biligma Oyola <sandylykova94@mail.ru>
2021-08-10 12:44:08 -0400

    added second solution

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -17,3 +17,16 @@ var searchInsert = function(nums, target) {
 
   return i;
 };
+
+// Solution 2 O(log(n)) time | O(1) space
+
+var searchInsert = function(nums, target) {
+  let left = 0, right = nums.length - 1;
+  while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) return mid;
+      else if (nums[mid] < target) left = mid + 1;
+      else right = mid - 1;
+  }
+  return left;
+};
